ce13e9990b6cb0189f27ba1a98f299134bccbbb0
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-04-15 17:51:12 +0600

    profit page done

--- src/Components/Cards/ProfitCards.jsx
+++ src/Components/Cards/ProfitCards.jsx
@@ -1,15 +1,15 @@
-const ProfitCards = () => {
+const ProfitCards = ({ profit }) => {
   return (
     <div className="card card-md shadow-md shadow-fourth">
       <div className="card-body">
-        <h2 className="text-xl font-semibold text-center">Mining Profit</h2>
+        <h2 className="text-xl font-semibold text-center">{profit.title}</h2>
         <h4 className="border border-white text-center py-1 mt-3 font-semibold text-[#6CF351]">
-          $758.75
+          {profit.amount}
         </h4>
-        <h3 className="text-center text-lg font-semibold ">$91-130/Day</h3>
+        <h3 className="text-center text-lg font-semibold ">{profit.time} </h3>
         <div className="justify-center card-actions">
           <button className="py-2 bg-ninth px-6 my-3 rounded-xl text-white font-bold">
-            Redeem Now
+            {profit.btnText}
           </button>
         </div>
       </div>
--- src/Pages/Profite/Profit.jsx
+++ src/Pages/Profite/Profit.jsx
@@ -2,14 +2,43 @@ import ProfitCards from "../../Components/Cards/ProfitCards";
 import ProfitAvater from "./ProfitAvater";
 
 const Profit = () => {
+  const profitData = [
+    {
+      id: 1,
+      title: "Mining Profit",
+      amount: "$758.75",
+      time: "$91-130/Day",
+      btnText: "Reddem Now",
+    },
+    {
+      id: 2,
+      title: "Boost",
+      amount: "10%",
+      time: "Next Stage",
+      btnText: "🠪",
+    },
+    {
+      id: 3,
+      title: "Affiliate Profit",
+      amount: "$758.75",
+      time: "$95/Day",
+      btnText: "Claim Now",
+    },
+    {
+      id: 4,
+      title: "Business Profit",
+      amount: "$758.75",
+      time: "$100+ $25/Day",
+      btnText: "Claim Now",
+    },
+  ];
   return (
     <div>
       <ProfitAvater />
       <div className="grid grid-cols-2 gap-8  mx-4 mt-8 pb-12">
-        <ProfitCards />
-        <ProfitCards />
-        <ProfitCards />
-        <ProfitCards />
+        {profitData.map((profit) => (
+          <ProfitCards key={profit.id} profit={profit} />
+        ))}
       </div>
     </div>
   );
