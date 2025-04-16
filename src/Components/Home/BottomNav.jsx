b0e60c9d9a9eec44c08a943e991e78e70d0f19be
Arif-Islam21 <arif.sojib2020@gmail.com>
2025-04-16 19:42:22 +0600

    bottom nav buttons updated

--- src/Components/Home/BottomNav.jsx
+++ src/Components/Home/BottomNav.jsx
@@ -2,33 +2,49 @@ import { CiPower } from "react-icons/ci";
 import { FaCoins, FaWallet } from "react-icons/fa";
 import { FaUserGroup } from "react-icons/fa6";
 import { BsLightningChargeFill } from "react-icons/bs";
+import { Link } from "react-router";
+import { useState } from "react";
 
 const BottomNav = () => {
+  const [isActive, setIsActive] = useState(false);
   return (
-    <div class="bg-button-bg relative w-full  mx-auto rounded-t-lg pb-4 mt-12 px-4 text-white">
-      <button class="absolute cursor-pointer bg-second rounded-full p-2 top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
-        <div class="rounded-full bg-gray-500 border-4 border-white shadow-xl w-16 h-16 flex items-center justify-center">
+    <div className="bg-button-bg relative w-full  mx-auto rounded-t-lg pb-4 mt-12 px-4 text-white">
+      <button
+        onClick={() => setIsActive(!isActive)}
+        className="absolute cursor-pointer bg-second rounded-full p-2 top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
+      >
+        <div
+          className={`rounded-full ${
+            isActive ? "bg-button-bg " : "bg-gray-500 "
+          } border shadow-xl w-16 border-white h-16 flex items-center justify-center`}
+        >
           <CiPower size={28} />
         </div>
       </button>
 
-      <div class="flex justify-around items-center mt-8 pt-4 border-t border-first">
-        <button class="flex flex-col cursor-pointer items-center">
+      <div className="flex justify-around items-center mt-8 pt-4 border-t border-first">
+        <Link
+          to="/profit"
+          className="flex flex-col cursor-pointer items-center"
+        >
           <FaCoins size={28} />
-          <span class="text-sm">Profit</span>
-        </button>
-        <button class="flex flex-col items-center cursor-pointer">
+          <span className="text-sm">Profit</span>
+        </Link>
+        <button className="flex flex-col items-center cursor-pointer">
           <FaUserGroup size={28} />
-          <span class="text-sm">Group</span>
+          <span className="text-sm">Group</span>
         </button>
-        <button class="flex flex-col items-center cursor-pointer">
+        <Link to="/boost" className="flex flex-col items-center cursor-pointer">
           <BsLightningChargeFill size={28} />
-          <span class="text-sm">Boost</span>
-        </button>
-        <button class="flex flex-col items-center cursor-pointer">
+          <span className="text-sm">Boost</span>
+        </Link>
+        <Link
+          to="/wallet"
+          className="flex flex-col items-center cursor-pointer"
+        >
           <FaWallet size={28} />
-          <span class="text-sm">Wallet</span>
-        </button>
+          <span className="text-sm">Wallet</span>
+        </Link>
       </div>
     </div>
   );
